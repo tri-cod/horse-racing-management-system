03fe2e033487dc59c64db7401a32674c2dbb2405
tranminhtinh <tranminhtinh3804@gmail.com>
2026-05-30 12:45:26 +0700

    fixbug login more

--- frontend/horse-racing-management-FE/src/api/authService.js
+++ frontend/horse-racing-management-FE/src/api/authService.js
@@ -12,3 +12,16 @@ export async function register(data) {
     }
     return result;
 }
+
+export async function login(data) {
+    const response = await fetch(`${BASE_URL}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    if (!response.ok) {
+        throw new Error(result.message || "Đăng nhập thất bại");
+    }
+    return result;
+}
--- frontend/horse-racing-management-FE/src/main.jsx
+++ frontend/horse-racing-management-FE/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Register from './auth/register.jsx'
+import Login from './auth/login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Register />
+    < Login/>
   </StrictMode>,
 )
